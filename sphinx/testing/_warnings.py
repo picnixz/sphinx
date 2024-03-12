d9dcb733de638c2bb1f8add8f1513756fa138689
Bénédikt Tran <10796600+picnixz@users.noreply.github.com>
2024-03-12 11:48:57 +0100

    Remove unnecessary classes.

--- sphinx/testing/_warnings.py
+++ sphinx/testing/_warnings.py
@@ -3,12 +3,7 @@
 from __future__ import annotations
 
 __all__ = [
-    'SphinxTestingWarning',
-    'NodeWarning',
-    'MarkWarning',
-    'MarkDeprecationWarning',
-    'FixtureWarning',
-    'FixtureDeprecationWarning',
+    'SphinxTestingWarning', 'NodeWarning', 'MarkWarning', 'FixtureWarning',
 ]
 
 from _pytest.warning_types import PytestWarning
@@ -16,15 +11,6 @@ from _pytest.warning_types import PytestWarning
 
 class SphinxTestingWarning(PytestWarning):
     """Base class for warnings emitted during test configuration."""
-
-
-class _DeprecationMixin:
-    def __init__(self, *args: object, removed_in: tuple[int, ...], **kwargs: object) -> None:
-        super().__init__(*args, **kwargs)
-        self.removed_in = '.'.join(map(str, removed_in))
-
-    def __str__(self) -> str:
-        return f'{super().__str__()} (will be removed in Sphinx {self.removed_in})'
 
 
 class NodeWarning(SphinxTestingWarning):
@@ -39,17 +25,9 @@ class MarkWarning(NodeWarning):
         super().__init__(message)
 
 
-class MarkDeprecationWarning(_DeprecationMixin, MarkWarning, PendingDeprecationWarning):
-    """A deprecation warning emitted in a marker."""
-
-
 class FixtureWarning(NodeWarning):
     """A warning emitted during a fixture configuration."""
 
     def __init__(self, message: str, fixturename: str | None = None) -> None:
         message = f'FIXTURE({fixturename!r}): {message}' if fixturename else message
         super().__init__(message)
-
-
-class FixtureDeprecationWarning(_DeprecationMixin, FixtureWarning, PendingDeprecationWarning):
-    """A deprecation warning emitted in a fixture."""
